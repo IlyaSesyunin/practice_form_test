25c01446a928aaa191ad36009a7eed0cbf3620cb
Ilya Sesyunin <sesyunin.ilya@gmail.com>
2024-04-27 20:38:57 +0500

    refactor

--- demoqa_tests/model/pages/registration_page.py
+++ demoqa_tests/model/pages/registration_page.py
@@ -8,62 +8,39 @@ class RegistrationPage:
     def open(self):
         browser.open('/automation-practice-form')
 
-    def fill_first_name(self, value):
-        browser.element('#firstName').type(value)
-
-    def fill_last_name(self, value):
-        browser.element('#lastName').type(value)
-
-    def fill_email(self, value):
-        browser.element('#userEmail').type(value)
-
-    def fill_gender(self):
+    def register_user(self, user):
+        browser.element('#firstName').type(user.first_name)
+        browser.element('#lastName').type(user.last_name)
+        browser.element('#userEmail').type(user.email)
         browser.element('[for="gender-radio-1"]').click()
-
-    def fill_number(self, value):
-        browser.element('#userNumber').type(value)
-
-    def fill_birthdate(self, year, month, date):
+        browser.element('#userNumber').type(user.phone_number)
         browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__month-select').click().element(f'option[value="{month}"]').click()
-        browser.element('.react-datepicker__year-select').click().element(f'option[value="{year}"]').click()
-        browser.element(f'.react-datepicker__day.react-datepicker__day--00{date}').click()
-
-    def fill_subject(self, value):
-        browser.element('#subjectsInput').type(value).press_enter()
+        browser.element('.react-datepicker__month-select').click().element(f'option[value="{user.month}"]').click()
+        browser.element('.react-datepicker__year-select').click().element(f'option[value="{user.year}"]').click()
+        browser.element(f'.react-datepicker__day.react-datepicker__day--00{user.day}').click()
+        browser.element('#subjectsInput').type(user.subjects).press_enter()
         browser.element('#subjectsInput').perform(command.js.scroll_into_view)
-
-    def fill_hobbies(self, value):
-        browser.element(f'[for="hobbies-checkbox-{value}"]').click()
-
-    def upload_picture(self, name):
-        browser.element('#uploadPicture').set_value(resource.path(name))
-
-    def fill_current_address(self, value):
-        browser.element('#currentAddress').type(value)
-
-    def fill_state(self, value='NCR'):
-        browser.element('#react-select-3-input').type(value).press_enter()
-
-    def fill_city(self, value='Delhi'):
-        browser.element('#react-select-4-input').type(value).press_enter()
-
-    def submit_form(self):
+        browser.element(f'[for="hobbies-checkbox-{user.hobbies}"]').click()
+        browser.element('#uploadPicture').set_value(resource.path(user.photo))
+        browser.element('#currentAddress').type(user.address)
+        browser.element('#react-select-3-input').type(user.state).press_enter()
+        browser.element('#react-select-4-input').type(user.city).press_enter()
         browser.element('#submit').click()
 
-    def assert_registered_info(self, name, email, sex, number, birthday, subject, hobby, file_name, address,
-                               state_and_city):
-        browser.element('.table').all('td').even.should(have.exact_texts(name,
-                                                                         email,
-                                                                         sex,
-                                                                         number,
-                                                                         birthday,
-                                                                         subject,
-                                                                         hobby,
-                                                                         file_name,
-                                                                         address,
-                                                                         state_and_city
-                                                                         ))
+    def assert_registered_info(self, user):
+        browser.element('.table').all('td').even.should(have.exact_texts(
+            f'{user.first_name} {user.last_name}',
+            user.email,
+            user.gender,
+            user.phone_number,
+            '01 September,1995',
+            user.subjects,
+            'Sports',
+            user.photo,
+            user.address,
+            f'{user.state} {user.city}'
+        )
+        )
 
 
 registration_page = RegistrationPage()
--- tests/test_form_demoqa.py
+++ tests/test_form_demoqa.py
@@ -1,33 +1,25 @@
 from demoqa_tests.model.pages.registration_page import registration_page
-
+from demoqa_tests.data.users import User
 
 
 def test_correct_full_form():
-    registration_page.open()
-    registration_page.fill_first_name('Ivan')
-    registration_page.fill_last_name('Ivanov')
-    registration_page.fill_email('ivan.ivanov@gmail.com')
-    registration_page.fill_gender()
-    registration_page.fill_number('1234567890')
-    registration_page.fill_birthdate(1995, 8, 1)
-    registration_page.fill_subject('comp')
-    registration_page.fill_hobbies(3)
-    registration_page.fill_hobbies(2)
-    registration_page.upload_picture('image.png')
-    registration_page.fill_current_address('address')
-    registration_page.fill_state('hary')
-    registration_page.fill_city('karn')
-    registration_page.submit_form()
-
-    registration_page.assert_registered_info(
-        'Ivan Ivanov',
-        'ivan.ivanov@gmail.com',
-        'Male',
-        '1234567890',
-        '01 September,1995',
-        'Computer Science',
-        'Music, Reading',
-        'image.png',
-        'address',
-        'Haryana Karnal'
+    user_ivan = User(
+        first_name='Ivan',
+        last_name='Ivanov',
+        email='ivan.ivanov@gmail.com',
+        gender='Male',
+        phone_number='1234567890',
+        year='1995',
+        month='8',
+        day='1',
+        subjects='Computer Science',
+        hobbies='1',
+        photo='image.png',
+        address='address',
+        state='Haryana',
+        city='Karnal'
     )
+
+    registration_page.open()
+    registration_page.register_user(user_ivan)
+    registration_page.assert_registered_info(user_ivan)
