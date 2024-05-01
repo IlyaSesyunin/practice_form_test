9242642746a45efad5953d2e78d25ca672c4d861
Ilya Sesyunin <sesyunin.ilya@gmail.com>
2024-05-01 13:44:05 +0500

    fix allure steps

--- tests/test_form_demoqa.py
+++ tests/test_form_demoqa.py
@@ -1,7 +1,10 @@
+import allure
 from demoqa_tests.model.pages.registration_page import registration_page
 from demoqa_tests.data.users import User
 
 
+@allure.tag('web')
+@allure.title('Successful fill form')
 def test_correct_full_form():
     user_ivan = User(
         first_name='Ivan',
@@ -20,6 +23,9 @@ def test_correct_full_form():
         city='Karnal'
     )
 
-    registration_page.open()
-    registration_page.register_user(user_ivan)
-    registration_page.assert_registered_info(user_ivan)
+    with allure.step('Open registration page'):
+        registration_page.open()
+    with allure.step('Fill in user data'):
+        registration_page.register_user(user_ivan)
+    with allure.step('Checking user data'):
+        registration_page.assert_registered_info(user_ivan)
